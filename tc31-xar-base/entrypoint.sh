10611b197d0a1d5254b128805b33139991b1c461
Michael Helfenstein <michael.helfenstein@noser.com>
2026-05-27 08:44:01 +0200

    add TC_RUN_MODE TEST

--- tc31-xar-base/entrypoint.sh
+++ tc31-xar-base/entrypoint.sh
@@ -8,10 +8,13 @@ set -e
 echo "Starting TcSystemServiceUm..."
 
 # Replaces the shell process with the TcSystemServiceUm process, ensuring proper signal handling
-# 0x4 is FastAsPossible, 0x7 should be realtime
+# 0x4 is FastAsPossible, 0x5 is Test, 0x7 should be realtime
 if [ "$TC_RUN_MODE" = 'FAST_AS_POSSIBLE' ];  then
     echo "Starting TcSystemServiceUm FastAsPossible"
     exec /usr/bin/TcSystemServiceUm -f 0x4 -i "${AMS_NETID}" -p /var/run/TcSystemServiceUm.pid
+elif [ "$TC_RUN_MODE" = 'TEST' ]; then
+    echo "Starting TcSystemServiceUm Test"
+    exec /usr/bin/TcSystemServiceUm -f 0x5 -i "${AMS_NETID}" -p /var/run/TcSystemServiceUm.pid
 else
     # $TC_RUN_MODE = REALTIME
     echo "Starting TcSystemServiceUm Realtime"
